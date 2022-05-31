4472a85450d757e439aef2b496d01b407f5e7098
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-05-31 10:16:15 -0300

    aula 15

---
+++ src/main/java/com/loiane/model/Course.java
@@ -0,0 +1,25 @@
+package com.loiane.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", length = 200, nullable = false)
+    private String name;
+
+    @Column(name = "category", length = 200, nullable = false)
+    private String category;
+
+}
